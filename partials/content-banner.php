f4ab7e2f1b0750cfbf9ceee7fad2910bcab286b1
smolanovsergey <smolanovsergey@gmail.com>
2022-11-15 14:19:18 +0200

    video

--- partials/content-banner.php
+++ partials/content-banner.php
@@ -8,6 +8,11 @@
     <div class="banner__rt banner_absolute">
         <img src="<?php echo get_template_directory_uri() . '/img/banner/rt.jpg' ?>" alt="rt">
     </div>
+    <div class="banner__button rotation">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri() . '/img/banner/button.svg' ?>" alt="center">
+        </a>
+    </div>
     <div class="banner__center">
         <img src="<?php echo get_template_directory_uri() . '/img/banner/center.jpg' ?>" alt="center">
     </div>
--- partials/content-video.php
+++ partials/content-video.php
@@ -1,7 +1,7 @@
 <section class="video">
     <div class="video__player">
         <img src="<?php echo get_template_directory_uri() . '/img/video/bg.jpg' ?>" alt="center">
-        <a href="#" class="video__play">
+        <a href="#" class="video__play rotation">
             <img src="<?php echo get_template_directory_uri() . '/img/video/play.svg' ?>" alt="center">
         </a>
     </div>
