ed5f63ddab597b6e85a973c7b518c69a8b30ca36
smolanovsergey <smolanovsergey@gmail.com>
2022-11-21 11:43:26 +0200

    footer

--- footer.php
+++ footer.php
@@ -11,9 +11,34 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-
-	</footer><!-- #colophon -->
+	<footer id="footer" class="footer">
+        <div class="footer__container big-container">
+            <div class="footer__branding">
+                <a class="footer__logo" href="<?php echo get_home_url(); ?>">
+                    <img src="<?php echo get_template_directory_uri() . '/img/footer/logo.png'?>" alt="footer logo">
+                </a>
+                <p class="footer__desc">ТМ АВГУСТ — український виробник широкого асортименту харчових інгредієнтів для кондитерської, хлібопекарської, молочної промисловості та виробництва морозива.</p>
+                <p class="footer__done">Розроблено в TH</p>
+                <p class="footer__copyright">ТМ «Август». All right reserved. 2021</p>
+            </div>
+            <nav class="footer__nav">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'footer-menu',
+                        'menu_id'        => 'footer-menu',
+                    )
+                );
+                ?>
+            </nav>
+            <div class="footer__contacts">
+                <a class="footer__phone footer__link" href="tel:+38 (099) 782-73-20">+38 (099) 782-73-20</a>
+                <p>Пн - Пт 09:00 - 18:00</p>
+                <a class="footer__mail footer__link" href="mailto:tm.avgust@gmail.com">tm.avgust@gmail.com</a>
+                <p>61017, Україна, м. Харків, вул. Лозівська 5</p>
+            </div>
+        </div>
+	</footer>
 
 <?php wp_footer(); ?>
 
