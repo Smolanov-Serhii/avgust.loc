67ede06f6a8ab54d35e9d050b7b0d2ab5c804103
smolanovsergey <smolanovsergey@gmail.com>
2022-12-14 10:24:05 +0200

    рецепт страница

--- single-recipes.php
+++ single-recipes.php
@@ -1,4 +1,29 @@
+<?php
+/* Template Name: Рецепты */
 
-<section class="recipes-single">
+get_header();
+$post_id = get_the_ID();
+?>
 
-</section>
+	<main id="main" class="main">
+
+        <section class="recipes-single">
+            <div class="recipes__container main-container">
+                <h1>
+                    <?php echo the_field("zagolovok_stranycz", $post_id); ?>
+                </h1>
+                <div class="recipes__banner">
+                    <?php the_post_thumbnail();?>
+                    <a href="<?php echo get_home_url() . '/products'; ?>" class="recipes__lnk rotation">
+                        <img src="<?php echo get_template_directory_uri() . '/img/page-recipes/shtamp.svg' ?>" alt="center">
+                    </a>
+                </div>
+            </div>
+        </section>
+
+	</main>
+
+<?php
+
+get_footer();
+
