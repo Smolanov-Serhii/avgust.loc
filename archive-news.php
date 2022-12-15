9f1380ba9c0477b6062f6c83891bf604e8b1cd1b
smolanovsergej <smolanovsergey@gmail.com>
2022-12-15 18:03:44 +0200

    ноут

---
+++ archive-news.php
@@ -0,0 +1,19 @@
+<?php
+/* Template Name: Новини */
+
+get_header();
+?>
+
+    <main id="main" class="main">
+
+        <section class="page-news">
+            <div class="page-news__container main-container">
+
+            </div>
+        </section>
+
+    </main>
+
+<?php
+
+get_footer();
