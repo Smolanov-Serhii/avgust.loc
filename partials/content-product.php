6d64943930c7aeb1695ae31b1cb986281fc0fdff
Сергей <smolanovsergey@gmail.com>
2023-02-11 20:50:34 +0200

    Changes

--- partials/content-product.php
+++ partials/content-product.php
@@ -13,11 +13,24 @@
         </div>
         <div class="product__content">
             <div class="product__content-left">
-                <img class="product__content-tl" src="<?php echo get_template_directory_uri() . '/img/product/tl.jpg' ?>" alt="center" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="200">
-                <img class="product__content-bl" src="<?php echo get_template_directory_uri() . '/img/product/bl.jpg' ?>" alt="center"data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="400">
+                <div class="product__content-tl">
+                    <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo get_template_directory_uri() . '/img/product/tl.jpg' ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="200">
+                        <!--                    <source data-src="--><?php //echo get_template_directory_uri() . '/video/15.mov' ?><!--" type="video/webm">-->
+                        <source data-src="<?php echo get_template_directory_uri() . '/video/15.mp4' ?>" type="video/mp4">
+                    </video>
+                </div>
+                <div class="product__content-bl">
+                    <video class="lazy lazy-video " autoplay muted loop playsinline poster="<?php echo get_template_directory_uri() . '/img/product/tl.jpg' ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="400">
+                        <!--                    <source data-src="--><?php //echo get_template_directory_uri() . '/video/15.mov' ?><!--" type="video/webm">-->
+                        <source data-src="<?php echo get_template_directory_uri() . '/video/15.mp4' ?>" type="video/mp4">
+                    </video>
+                </div>
             </div>
             <div class="product__content-right">
-                <img src="<?php echo get_template_directory_uri() . '/img/product/big.jpg' ?>" alt="center" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="700">
+                <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo get_template_directory_uri() . '/img/product/big.jpg' ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="700">
+<!--                    <source data-src="--><?php //echo get_template_directory_uri() . '/video/21.mov' ?><!--" type="video/webm">-->
+                    <source data-src="<?php echo get_template_directory_uri() . '/video/21.mp4' ?>" type="video/mp4">
+                </video>
                 <div class="product__desc" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="900">
                     <p>Компанія август пропонує високоякісні інгредієнти власного виробництва для наступних галузей харчової промисловості:</p>
                 </div>
