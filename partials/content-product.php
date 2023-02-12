71b25f734948ab26af80a26482fbe503614cc286
Сергей <smolanovsergey@gmail.com>
2023-02-12 09:29:56 +0200

    Changes

--- partials/content-product.php
+++ partials/content-product.php
@@ -1,38 +1,81 @@
+<?php
+$post_id = get_the_ID();
+?>
 <section class="product">
     <div class="product__container main-container">
         <div class="product__header">
             <div class="product__name">
-                <a href="<?php echo get_home_url() . '/russkij-kompaniya'; ?>" class="about__marker" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200">
-                    Про компанію
+                <a href="<?php echo the_field('sslka_na_marker', $post_id) ?>" class="about__marker" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200">
+                    <?php echo the_field('tekst_markera', $post_id) ?>
                 </a>
             </div>
             <h2 class="product__title section-title">
-                <span data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200">Ми виробляємо великий</span>
-                <span data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">асортимент <i>продукції</i></span>
+                <?php echo the_field('zagolovok_bloka_tovar_s_vydeo_1', $post_id) ?>
             </h2>
         </div>
         <div class="product__content">
             <div class="product__content-left">
-                <div class="product__content-tl">
-                    <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo get_template_directory_uri() . '/img/product/tl.jpg' ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="200">
-                        <!--                    <source data-src="--><?php //echo get_template_directory_uri() . '/video/15.mov' ?><!--" type="video/webm">-->
-                        <source data-src="<?php echo get_template_directory_uri() . '/video/15.mp4' ?>" type="video/mp4">
-                    </video>
-                </div>
-                <div class="product__content-bl">
-                    <video class="lazy lazy-video " autoplay muted loop playsinline poster="<?php echo get_template_directory_uri() . '/img/product/tl.jpg' ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="400">
-                        <!--                    <source data-src="--><?php //echo get_template_directory_uri() . '/video/15.mov' ?><!--" type="video/webm">-->
-                        <source data-src="<?php echo get_template_directory_uri() . '/video/15.mp4' ?>" type="video/mp4">
-                    </video>
-                </div>
+                <?php
+                $counter = 1;
+                $post_objects = get_field('vberyte_try_tovara_dlya_otobrazhenyya', $post_id);
+                if( $post_objects ): ?>
+                    <?php foreach( $post_objects as $post): ?>
+                        <?php setup_postdata($post); ?>
+                        <?php
+                        $postpers_id = get_the_ID();
+                        $img = get_field('oblozhka_dlya_vydeo', $postpers_id);
+                        $video = get_field('vydeo_fajl_mp4', $postpers_id);
+                        if($counter == 1){
+                            ?>
+                            <a href="<?php the_permalink();?>" class="product__content-tl">
+                                <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo $img;?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="200">
+                                    <source data-src="<?php echo $video;?>" type="video/mp4">
+                                </video>
+                            </a>
+                            <?php
+                        }
+                        if($counter == 2){
+                            ?>
+                            <a href="<?php the_permalink();?>" class="product__content-bl">
+                                <video class="lazy lazy-video " autoplay muted loop playsinline poster="<?php echo $img;?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="400">
+                                    <source data-src="<?php echo $video;?>" type="video/mp4">
+                                </video>
+                            </a>
+                            <?php
+                        }
+                        $counter ++;
+                        endforeach; ?>
+                    <?php wp_reset_postdata(); // ВАЖНО - сбросьте значение $post object чтобы избежать ошибок в дальнейшем коде ?>
+                <?php endif;
+                ?>
             </div>
             <div class="product__content-right">
-                <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo get_template_directory_uri() . '/img/product/big.jpg' ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="700">
-<!--                    <source data-src="--><?php //echo get_template_directory_uri() . '/video/21.mov' ?><!--" type="video/webm">-->
-                    <source data-src="<?php echo get_template_directory_uri() . '/video/21.mp4' ?>" type="video/mp4">
-                </video>
+                <?php
+                $counter = 1;
+                $post_objects = get_field('vberyte_try_tovara_dlya_otobrazhenyya', $post_id);
+                if( $post_objects ): ?>
+                    <?php foreach( $post_objects as $post): ?>
+                        <?php setup_postdata($post); ?>
+                        <?php
+                        $postpers_id = get_the_ID();
+                        $img = get_field('oblozhka_dlya_vydeo', $postpers_id);
+                        $video = get_field('vydeo_fajl_mp4', $postpers_id);
+                        if($counter == 3){
+                            ?>
+                            <a href="<?php the_permalink();?>">
+                                <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo $img;?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="700">
+                                    <source data-src="<?php echo $video;?>" type="video/mp4">
+                                </video>
+                            </a>
+                            <?php
+                        }
+                        $counter ++;
+                    endforeach; ?>
+                    <?php wp_reset_postdata(); // ВАЖНО - сбросьте значение $post object чтобы избежать ошибок в дальнейшем коде ?>
+                <?php endif;
+                ?>
                 <div class="product__desc" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="900">
-                    <p>Компанія август пропонує високоякісні інгредієнти власного виробництва для наступних галузей харчової промисловості:</p>
+                    <p><?php echo the_field('opysanye_snyzu_pod_vydeo', $post_id) ?></p>
                 </div>
             </div>
         </div>
--- single-products.php
+++ single-products.php
@@ -55,67 +55,79 @@ $post_id = get_the_ID();
                 </div>
             </div>
         </section>
-        <section class="product-else">
-            <div class="product-else__container main-container">
-                <div class="product__w50">
-                    <a href="" class="about__marker">
-                        <?php echo the_field("nadpis_sleva_ot_bloka", $post_id); ?>
-                    </a>
-                </div>
-                <div class="product-else__wrapper product__w50">
-                    <h2 class="product__block-title block-title">
-                        <?php echo the_field("zagolovok_bloku_smaky", $post_id); ?>
-                    </h2>
-                    <div class="product__block">
-                        <?php
-                        if( have_rows('perelik_smakiv') ):
-                            while( have_rows('perelik_smakiv') ) : the_row();
-                                $title = get_sub_field('nazva_produktu');
-                                $img = get_sub_field('zobrazhennya_produktu');
-                                $lnk = get_sub_field('posylannya_na_zapys');
-                                ?>
-                                <p class="product__block-item">
-                                    <?php
-                                        if($lnk){
-                                            ?>
-                                            <a href="<?php echo $lnk; ?>">
+        <?php
+            if(get_field('otobrazyt_blok_smaky', $post_id) == 'Да'){
+               ?>
+                <section class="product-else">
+                    <div class="product-else__container main-container">
+                        <div class="product__w50">
+                            <a href="" class="about__marker">
+                                <?php echo the_field("nadpis_sleva_ot_bloka", $post_id); ?>
+                            </a>
+                        </div>
+                        <div class="product-else__wrapper product__w50">
+                            <h2 class="product__block-title block-title">
+                                <?php echo the_field("zagolovok_bloku_smaky", $post_id); ?>
+                            </h2>
+                            <div class="product__block">
+                                <?php
+                                if( have_rows('perelik_smakiv') ):
+                                    while( have_rows('perelik_smakiv') ) : the_row();
+                                        $title = get_sub_field('nazva_produktu');
+                                        $img = get_sub_field('zobrazhennya_produktu');
+                                        $lnk = get_sub_field('posylannya_na_zapys');
+                                        ?>
+                                        <p class="product__block-item">
+                                            <?php
+                                            if($lnk){
+                                                ?>
+                                                <a href="<?php echo $lnk; ?>">
+                                                    <?php echo $title; ?>
+                                                </a>
+                                                <?php
+                                            }
+                                            else{
+                                                ?>
                                                 <?php echo $title; ?>
-                                            </a>
-                                            <?php
-                                        }
-                                        else{
+                                                <?php
+                                            }
                                             ?>
-                                            <?php echo $title; ?>
-                                            <?php
-                                        }
-                                    ?>
-                                </p>
-                            <?php
-                            endwhile;
-                        else :
-                        endif;
-                        ?>
+                                        </p>
+                                    <?php
+                                    endwhile;
+                                else :
+                                endif;
+                                ?>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </section>
-        <section class="product-about">
-            <div class="product-about__container main-container">
-                <div class="product__w50">
-                    <a href="" class="about__marker">
-                        <?php echo the_field("zagolovok_bloku_smaky", $post_id); ?>
-                    </a>
-                </div>
-                <div class="product-else__wrapper product__w50">
-                    <h2 class="product__block-title block-title">
-                        <?php echo the_field("zagolovok_opys_produktu", $post_id); ?>
-                    </h2>
-                    <div class="product-about__block product__block-column">
-                        <?php echo the_field("opys_v_dvi_kolonky_opys_produktu", $post_id); ?>
+                </section>
+               <?php
+            }
+        ?>
+        <?php
+        if(get_field('otobrazyt_blok_opysanye', $post_id) == 'Да'){
+            ?>
+                <section class="product-about">
+                    <div class="product-about__container main-container">
+                        <div class="product__w50">
+                            <a href="" class="about__marker">
+                                <?php echo the_field("zagolovok_bloku_smaky", $post_id); ?>
+                            </a>
+                        </div>
+                        <div class="product-else__wrapper product__w50">
+                            <h2 class="product__block-title block-title">
+                                <?php echo the_field("zagolovok_opys_produktu", $post_id); ?>
+                            </h2>
+                            <div class="product-about__block product__block-column">
+                                <?php echo the_field("opys_v_dvi_kolonky_opys_produktu", $post_id); ?>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </section>
+                </section>
+            <?php
+        }
+        ?>
             <?php
             if(get_field('fajl_video', $post_id)){
                 ?>
@@ -129,112 +141,131 @@ $post_id = get_the_ID();
                 <?php
             }
             ?>
-        <section class="product-bottom">
-            <div class="product-bottom__container main-container">
-                <h2 class="product-bottom__title">
-                    <?php echo the_field("zagolovok_opys_vnyzu", $post_id); ?>
-                </h2>
-                <div class="product-bottom__wrapper">
-                    <div class="product-bottom__block product__w50 product__block-column">
-                        <?php echo the_field("opys_v_2_kolonky_opys_vnyzu", $post_id); ?>
-                    </div>
-                    <div class="product-bottom-pluses product__w50">
-                        <?php echo the_field("perelik_perevag", $post_id); ?>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="interest">
-            <div class="interest__container main-container">
-                <div class="interest__header">
-                    <h2 class="interest__title section-title">
-                        <?php echo the_field("zagolovok_interesnye_reczepty", $post_id); ?>
-                    </h2>
-                    <div class="interest__marker">
-                        молочні термостабільні
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7.2776 0.886461L7.27763 2.14809L12.8918 2.14812L0.437911 14.602L1.33005 15.4941L13.7839 3.04026L13.7839 8.65436L15.0455 8.65439L15.0455 0.886431L7.2776 0.886461Z" fill="#B78D41"/>
-                        </svg>
-                    </div>
-                </div>
-                <div class="interest__list">
-                    <?php
-                    $post_objects = get_field('interesnye_reczepty_perechen', $post_id);
-                    if( $post_objects ): ?>
-                        <?php foreach( $post_objects as $post): ?>
-                            <?php setup_postdata($post); ?>
-                            <?php
-                            $postpers_id = get_the_ID();
-                            $image = get_field('kvadratne_zobrazhennya_dlya_czikavi_reczepty', $postpers_id);
-                            $desc = get_field('kratkoe_opysanye_na_rozvodyashhuyu', $postpers_id);
-                            ?>
-                            <div class="interest__item">
-                                <div class="interest__item-image">
-                                    <img src="<?php echo $image; ?>" alt="<?php the_title();?>">
-                                </div>
-                                <div class="interest__item-content">
-                                    <h3 class="interest__item-title">
-                                        <?php the_title();?>
-                                    </h3>
-                                    <p class="interest__item-desc">
-                                        <?php echo $desc;?>
-                                    </p>
-                                    <a href="" class="interest__item-lnk">
-                                        <?php echo the_field('tekst_chitati_dali', 'option') ?>
-                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M7.2776 0.886461L7.27763 2.14809L12.8918 2.14812L0.437911 14.602L1.33005 15.4941L13.7839 3.04026L13.7839 8.65436L15.0455 8.65439L15.0455 0.886431L7.2776 0.886461Z" fill="#B78D41"/>
-                                        </svg>
-                                    </a>
-                                </div>
+        <?php
+            if(get_field('otobrazyt_opysanye_vnyzu', $post_id) == 'Да'){
+               ?>
+                <section class="product-bottom">
+                    <div class="product-bottom__container main-container">
+                        <h2 class="product-bottom__title">
+                            <?php echo the_field("zagolovok_opys_vnyzu", $post_id); ?>
+                        </h2>
+                        <div class="product-bottom__wrapper">
+                            <div class="product-bottom__block product__w50 product__block-column">
+                                <?php echo the_field("opys_v_2_kolonky_opys_vnyzu", $post_id); ?>
                             </div>
-                        <?php endforeach; ?>
-                        <?php wp_reset_postdata(); // ВАЖНО - сбросьте значение $post object чтобы избежать ошибок в дальнейшем коде ?>
-                    <?php endif;
-                    ?>
-                </div>
-            </div>
-        </section>
-        <section class="popular">
-            <div class="popular__container full-container">
-                <div class="popular__header main-container">
-                    <div class="about__marker product__w50">
-                        <?php echo the_field("tekst_sleva_populyarnaya_produkcziya", $post_id); ?>
+                            <div class="product-bottom-pluses product__w50">
+                                <?php echo the_field("perelik_perevag", $post_id); ?>
+                            </div>
+                        </div>
                     </div>
-                    <h2 class="popular__title section-title product__w50">
-                        <?php echo the_field("zagolovok_populyarnaya_produkcziya", $post_id); ?>
-                    </h2>
-                </div>
-                <div class="popular__list swiper-container main-container">
-                    <div class="popular__wrapper swiper-wrapper">
+                </section>
+               <?php
+            }
+        ?>
+
+        <?php
+        if(get_field('otobrazyt_ynteresne_reczept', $post_id) == 'Да'){
+            ?>
+            <section class="interest">
+                <div class="interest__container main-container">
+                    <div class="interest__header">
+                        <h2 class="interest__title section-title">
+                            <?php echo the_field("zagolovok_interesnye_reczepty", $post_id); ?>
+                        </h2>
+                        <div class="interest__marker">
+                            молочні термостабільні
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.2776 0.886461L7.27763 2.14809L12.8918 2.14812L0.437911 14.602L1.33005 15.4941L13.7839 3.04026L13.7839 8.65436L15.0455 8.65439L15.0455 0.886431L7.2776 0.886461Z" fill="#B78D41"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="interest__list">
                         <?php
-                        $post_objects = get_field('populyarnaya_produkcziya', $post_id);
+                        $post_objects = get_field('interesnye_reczepty_perechen', $post_id);
                         if( $post_objects ): ?>
                             <?php foreach( $post_objects as $post): ?>
                                 <?php setup_postdata($post); ?>
                                 <?php
                                 $postpers_id = get_the_ID();
-                                $image = get_field('kartinka_na_rozvoryashhuyu_kvadratnaya', $postpers_id);
+                                $image = get_field('kvadratne_zobrazhennya_dlya_czikavi_reczepty', $postpers_id);
+                                $desc = get_field('kratkoe_opysanye_na_rozvodyashhuyu', $postpers_id);
                                 ?>
-                                <div class="popular__item swiper-slide">
-                                    <a href="<?php the_permalink();?>>">
-                                        <img class="popular__item-img" src="<?php echo $image; ?>" alt="<?php the_title();?>">
-                                        <h3 class="popular__item-title"><?php the_title();?></h3>
-                                    </a>
+                                <div class="interest__item">
+                                    <div class="interest__item-image">
+                                        <img src="<?php echo $image; ?>" alt="<?php the_title();?>">
+                                    </div>
+                                    <div class="interest__item-content">
+                                        <h3 class="interest__item-title">
+                                            <?php the_title();?>
+                                        </h3>
+                                        <p class="interest__item-desc">
+                                            <?php echo $desc;?>
+                                        </p>
+                                        <a href="" class="interest__item-lnk">
+                                            <?php echo the_field('tekst_chitati_dali', 'option') ?>
+                                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M7.2776 0.886461L7.27763 2.14809L12.8918 2.14812L0.437911 14.602L1.33005 15.4941L13.7839 3.04026L13.7839 8.65436L15.0455 8.65439L15.0455 0.886431L7.2776 0.886461Z" fill="#B78D41"/>
+                                            </svg>
+                                        </a>
+                                    </div>
                                 </div>
                             <?php endforeach; ?>
                             <?php wp_reset_postdata(); // ВАЖНО - сбросьте значение $post object чтобы избежать ошибок в дальнейшем коде ?>
                         <?php endif;
                         ?>
+                    </div>
                 </div>
-            </div>
-            <div class="news__bottom">
-                <div class="news__button rotation">
-                    <a href="<?php echo get_home_url() . '/products'; ?>">
-                        <img src="<?php echo get_template_directory_uri() . '/img/news/shtamp.svg' ?>" alt="center">
-                    </a>
-                </div>
-            </div>
-        </section>
+            </section>
+            <?php
+        }
+        ?>
+        <?php
+        if(get_field('otoyurazyt_populyarne_produkt', $post_id) == 'Да'){
+            ?>
+            <section class="popular">
+                <div class="popular__container full-container">
+                    <div class="popular__header main-container">
+                        <div class="about__marker product__w50">
+                            <?php echo the_field("tekst_sleva_populyarnaya_produkcziya", $post_id); ?>
+                        </div>
+                        <h2 class="popular__title section-title product__w50">
+                            <?php echo the_field("zagolovok_populyarnaya_produkcziya", $post_id); ?>
+                        </h2>
+                    </div>
+                    <div class="popular__list swiper-container main-container">
+                        <div class="popular__wrapper swiper-wrapper">
+                            <?php
+                            $post_objects = get_field('populyarnaya_produkcziya', $post_id);
+                            if( $post_objects ): ?>
+                                <?php foreach( $post_objects as $post): ?>
+                                    <?php setup_postdata($post); ?>
+                                    <?php
+                                    $postpers_id = get_the_ID();
+                                    $image = get_field('kartinka_na_rozvoryashhuyu_kvadratnaya', $postpers_id);
+                                    ?>
+                                    <div class="popular__item swiper-slide">
+                                        <a href="<?php the_permalink();?>>">
+                                            <img class="popular__item-img" src="<?php echo $image; ?>" alt="<?php the_title();?>">
+                                            <h3 class="popular__item-title"><?php the_title();?></h3>
+                                        </a>
+                                    </div>
+                                <?php endforeach; ?>
+                                <?php wp_reset_postdata(); // ВАЖНО - сбросьте значение $post object чтобы избежать ошибок в дальнейшем коде ?>
+                            <?php endif;
+                            ?>
+                        </div>
+                    </div>
+                    <div class="news__bottom">
+                        <div class="news__button rotation">
+                            <a href="<?php echo get_home_url() . '/products'; ?>">
+                                <img src="<?php echo get_template_directory_uri() . '/img/news/shtamp.svg' ?>" alt="center">
+                            </a>
+                        </div>
+                    </div>
+            </section>
+            <?php
+        }
+        ?>
 	</main>
 
 <?php
