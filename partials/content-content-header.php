441fde2d5130f07d1a4f0e26c4f3cc072355122a
Сергей <smolanovsergey@gmail.com>
2023-02-12 17:55:45 +0200

    Changes

--- partials/content-content-header.php
+++ partials/content-content-header.php
@@ -1,34 +1,58 @@
+<?php
+    $post_id = get_the_ID();
+?>
 <section class="content-header">
     <div class="content-header__container main-container">
         <div class="content-header__top">
-            <h1 class="content-header__title main-title">
-                <span>Про</span>
-                <span>компанію</span>
-                <span>ТМ Август</span>
+            <h1 class="content-header__title main-title" style="background-image: url('<?php echo get_template_directory_uri() . '/img/content-header/white.jpg' ?>')">
+                <?php echo the_field("zagolovok_v_baner", $post_id); ?>
             </h1>
             <div class="content-header__image">
                 <a class="content-header__button rotation" href="<?php echo get_home_url() . '/products'; ?>">
                     <img class="content-header__image-shtamp" src="<?php echo get_template_directory_uri() . '/img/content-header/shtamp.svg' ?>" alt="main">
                 </a>
-                <img class="content-header__image-main" src="<?php echo get_template_directory_uri() . '/img/content-header/main.jpg' ?>" alt="main">
+                <?php
+                    if (get_field("vydeo_esly_est", $post_id)){
+                        ?>
+                            <div class="content-header__image-main">
+                                <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo the_field("kartynka_v_banner", $post_id); ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="200">
+                                    <source data-src="<?php echo the_field("vydeo_esly_est", $post_id); ?>" type="video/mp4">
+                                </video>
+                            </div>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="content-header__image-main">
+                            <img src="<?php echo the_field("kartynka_v_banner", $post_id); ?>" alt="main">
+                        </div>
+                        <?php
+                    }
+                ?>
             </div>
         </div>
         <div class="content-header__bottom">
             <div class="content-header__bottom-image">
-                <img src="<?php echo get_template_directory_uri() . '/img/content-header/small.jpg' ?>" alt="small">
+                <div class="wrapper">
+                    <?php
+                    if (get_field("vydeo_esly_est", $post_id)){
+                        ?>
+                        <video class="lazy lazy-video" autoplay muted loop playsinline poster="<?php echo the_field("malenkoe_foto_sleva", $post_id); ?>" data-aos="zoom-out" data-aos-duration="1000" data-aos-delay="200">
+                            <source data-src="<?php echo the_field("mklenkoe_vydeo_sleva", $post_id); ?>" type="video/mp4">
+                        </video>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="content-header__image-main">
+                            <img src="<?php echo the_field("malenkoe_foto_sleva", $post_id); ?>" alt="small">
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
             </div>
             <div class="about__desc">
-                <div class="wrapper">
-                    <p>ТМ АВГУСТ — український виробник широкого асортименту харчових інгредієнтів для кондитерської, хлібопекарської, молочної промисловості та виробництва морозива.</p>
-                    <p>Компанія була заснована в 1998 році як підприємство європейського зразка для випуску високоякісних інгредієнтів для харчової промисловості.</p>
-                    <p>На сьогоднішній день це сучасне високотехнологічне виробництво </p>
-                </div>
-                <div class="wrapper">
-                    <p>оснащене новітнім обладнанням кращих європейських стандартів.</p>
-                    <p>Спеціалісти ТМ АВГУСТ досягли стабільно високої якості виробництва, що підтверджується успішною сертифікацією НАССР в 2013 році у відповідності з вимогами: </p>
-                    <p><strong>ISO 22000:2007</strong></p>
-                    <p><img src="<?php echo get_template_directory_uri() . '/img/about/about-digits.svg' ?>" alt="center"></p>
-                </div>
+                <?php echo the_field("opysanye_bloka_baner", $post_id); ?>
+                <p><img src="<?php echo get_template_directory_uri() . '/img/about/about-digits.svg' ?>" alt="center"></p>
             </div>
         </div>
     </div>
